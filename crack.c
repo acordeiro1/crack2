434d03ca870945e0a610b9e3655fb20185abf3c8
Ana Christine Cordeiro <acordeiro1@student.sierracollege.edu>
2024-10-27 17:11:58 -0700

    Updated main function

--- crack.c
+++ crack.c
@@ -67,28 +67,54 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // These two lines exist for testing. When you have
+    /*// These two lines exist for testing. When you have
     // tryWord working, it should display the hash for "hello",
     // which is 5d41402abc4b2a76b9719d911017c592.
     // Then you can remove these two lines and complete the rest
     // of the main function below.
     char *found = tryWord("hello", "hashes00.txt");
-    printf("%s %s\n", found, "hello");
-
+    printf("%s %s\n", found, "hello");*/
 
     // Open the dictionary file for reading.
-    
+    FILE *dictionaryFile = fopen(argv[2], "r");
+    if (!dictionaryFile)
+    {
+        printf("Can't open %s for writing\n", argv[2]);
+        exit(1);
+    }
 
     // For each dictionary word, pass it to tryWord, which
     // will attempt to match it against the hashes in the hash_file.
+    char word[PASS_LEN];
+    int crackedCount = 0;
     
     // If we got a match, display the hash and the word. For example:
     //   5d41402abc4b2a76b9719d911017c592 hello
+
+    while (fgets(word, sizeof(word), dictionaryFile) != NULL) 
+    {
+        size_t length = strlen(word);
+        if (length > 0 && word[length - 1] == '\n') 
+        {
+            word[length - 1] = '\0';
+        }
+
+        char *found = tryWord(word, argv[1]);
+        if (found != NULL) 
+        {
+            printf("%s %s\n", found, word);
+            free(found); // Free the duplicated hash
+            crackedCount++;
+        }
+    }
     
     // Close the dictionary file.
-
-    // Display the number of hashes that were cracked.
+    fclose(dictionaryFile);
     
-    // Free up any malloc'd memory?
+    // Display the number of hashes that were cracked.
+    printf("Total hashes cracked: %d\n", crackedCount);
+    
+    // Free up any malloc'd memory, there is not need because it was free'd after everytime used.
+    return 0;
 }
 
